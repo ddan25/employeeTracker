e5d6e1190d1b5fa9aada78262f54f74d3460bad1
ddan25 <peterortiz97@yahoo.com>
2024-10-10 18:34:30 -0700

    created viewDepartments, viewRoles, and viewEmployees functions

--- Assets/index.js
+++ Assets/index.js
@@ -10,6 +10,36 @@ async function connectDB() {
     await client.connect();
 }
 
+async function viewDepartments() {
+    const result = await client.query('SELECT * FROM departments');
+    console.table(result.rows);
+}
+
+async function viewRoles() {
+    const result = await client.query('SELECT * FROM roles');
+    console.table(result.rows);
+}
+
+async function viewEmployees() {
+    const result = await client.query('SELECT * FROM employees');
+    console.table(result.rows);
+}
+
+async function addDepartment() {
+    const { name } = await inquirer.prompt({
+        type: 'input',
+        name: 'name',
+        message: 'Enter the department name:',
+    });
+
+    const result = await client.query('INSERT INTO departments (name) VALUES ($1)', [name]);
+    console.log(`Department "${name}" added successfully.`);
+}
+
+async function addRole() {
+    
+}
+
 async function menu() {
     await inquirer.prompt({
         tyoe: 'list',
@@ -55,4 +85,12 @@ async function menu() {
         default:
             console.log('Invalid option. Please try again.');
     }
+    menu();
 }
+
+async function start() {
+    await connectDB();
+    menu();
+}
+
+start();
